2f84ab35630c9646a0dc25dc0e2d57694ed6e7a0
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-10-27 16:34:22 -0200

    Bot

--- src/App/Controllers/BotController.php
+++ src/App/Controllers/BotController.php
@@ -6,24 +6,31 @@ use App\Core\Http\Controllers\Controller;
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use Illuminate\Http\Request;
-use Maestro\Rest;
 
 class BotController extends Controller
 {
-    public function handle($request)
+    public function handle()
     {
         DriverManager::loadDriver(\BotMan\Drivers\BotFramework\BotFrameworkDriver::class);
 
         $config = [
-
+            'facebook' => [
+                'token' => 'EAAkDsm7f2PUBAJBK5CYdUWvu7FLrauOZCzZCpsIVN4e4hesG0agr4CVZBwWhFfqLXVTH2Ht7bqmIbZCZAc3uSYTeGISjV04mardwt2ktyS7kOMouhRIIoJ2yemZCUUM1hHmqyPc9QacSHrYLKwhsgKg6DbBJRKhODbKOL4S6aLQAZDZD',
+                'verification'=>'gy840pghr0euggju3woifhudsfp',
+            ]
         ];
 
         // create an instance
         $botman = BotManFactory::create($config);
 
         // give the bot something to listen for.
-        $botman->hears('hello', function (BotMan $bot) {
+        $botman->hears('Oi', function (BotMan $bot) {
             $bot->reply('Hello yourself.');
+        });
+
+        // give the bot something to listen for.
+        $botman->hears('Cole', function (BotMan $bot) {
+            $bot->reply('Hello yourself cole.');
         });
 
         // start listening
