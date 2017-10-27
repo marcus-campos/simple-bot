e4ecbaa3f96023dff92fabfa69d4af230d41588f
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-10-27 11:18:13 -0200

    Ignored

--- src/App/Controllers/BotController.php
+++ src/App/Controllers/BotController.php
@@ -3,13 +3,30 @@
 namespace SimpleBot\App\Controllers;
 
 use App\Core\Http\Controllers\Controller;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
 use Illuminate\Http\Request;
 use Maestro\Rest;
 
 class BotController extends Controller
 {
-    public function handle(Request $request)
+    public function handle($request)
     {
+        DriverManager::loadDriver(\BotMan\Drivers\BotFramework\BotFrameworkDriver::class);
 
+        $config = [
+
+        ];
+
+        // create an instance
+        $botman = BotManFactory::create($config);
+
+        // give the bot something to listen for.
+        $botman->hears('hello', function (BotMan $bot) {
+            $bot->reply('Hello yourself.');
+        });
+
+        // start listening
+        $botman->listen();
     }
 }
