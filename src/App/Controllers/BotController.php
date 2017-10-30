799d114e7d00ccb851b204470a312f509e62b2ff
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2017-10-30 20:16:36 -0200

    Update simplebot

--- src/App/Controllers/BotController.php
+++ src/App/Controllers/BotController.php
@@ -4,33 +4,20 @@ namespace SimpleBot\App\Controllers;
 
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
-use BotMan\BotMan\Drivers\DriverManager;
 
 class BotController
 {
-    public function handle()
+    public function handle(\Request $request)
     {
-        DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
-
-        $config = [
-            'facebook' => [
-                'token' => 'EAAkDsm7f2PUBACU7udZBd1MVIhZAVjYj6XJZBdeZAvXCDnUNjiDk6iJ43pedNYZAcCUy75qcOOxarXdFTbKylCwp1KjQi4LOkXNF2w8ZBvXBEON3eUW2GMl9M8UNqt4Qb7MxdFTTjru0ynpPJ5I49WlFkElXaMzvII1TZBz0T1jiwZDZD',
-                'app_secret' => 'd374631fa82a58cd7219ebb722c6dc01',
-                'verification'=>'gy840pghr0euggju3woifhudsfp'
-            ]
-        ];
-
-        // create an instance
-        $botman = BotManFactory::create($config);
-
+        $botman = app('botman');
         // give the bot something to listen for.
         $botman->hears('Oi', function (BotMan $bot) {
-            $bot->reply('Hello yourself.');
+            $bot->reply('Olá.');
         });
 
         // give the bot something to listen for.
         $botman->hears('Cole', function (BotMan $bot) {
-            $bot->reply('Hello yourself cole.');
+            $bot->reply('Cole.');
         });
 
         // start listening
